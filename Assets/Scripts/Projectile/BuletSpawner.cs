8f6e9f638b61b431f4d5414637654ecadeaea0af
erfan00725 <erfansedighi84@gmail.com>
2026-09-16 23:14:38 +0330

    add projectile limit to projectileSpawner

--- Assets/Scripts/Projectile/BuletSpawner.cs
+++ Assets/Scripts/Projectile/BuletSpawner.cs
@@ -1,12 +1,28 @@
+using System;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public class BuletSpawner : MonoBehaviour
 {
     public Projectile projectilePrefab;
+    
+    public int maxBulletsCount = 10;
+    
+    private int _currentBulletsCount = 0;
+
+    private void Awake()
+    {
+        _currentBulletsCount = maxBulletsCount;
+    }
 
     public Projectile InstantiateBullet()
     {
-        return Instantiate(projectilePrefab, transform.position, transform.rotation, gameObject.transform);
+        if (_currentBulletsCount > 0)
+        {
+            _currentBulletsCount--;
+            return Instantiate(projectilePrefab, transform.position, transform.rotation, gameObject.transform);
+        }
+        Debug.LogWarning("No bullets available");
+        return null;
     }
 }
